7c6c75e59ef3cd719836676f1892ec08ab85672f
Student User <student@workstation.lab.example.com>
2020-11-05 17:13:23 -0500

    changes of app file

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  res.send('Hello World!from DUBAI Soap is not from foam\n');
 });
 
 app.listen(8080, function () {
